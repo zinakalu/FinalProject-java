87b965bcec2cc27f125f0c109f2a4fbc878bb48b
zinakalu <zina.kalu@gmail.com>
2023-08-05 17:03:43 -0400

    commits

--- src/main/java/VA/com/example/virtual/assistant/ApiResponseRepository.java
+++ src/main/java/VA/com/example/virtual/assistant/ApiResponseRepository.java
@@ -1,9 +1,8 @@
 package VA.com.example.virtual.assistant;
 
-// import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApiResponseRepository extends JpaRepository<ApiResponse, Integer> {
-    // List<ApiResponse> findByUser_Name(String userName);
+    
 }
